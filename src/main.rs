95da7a109408a41e458e4b66aafbc9dc7ed8bffb
ethicnology <ethicnology@pm.me>
2024-10-14 09:05:02 -0400

    chore: CORS Any for dev purposes

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,19 @@ async fn main() {
 
     let app = Router::new()
         .route("/key", post(crate::handlers::store_key))
-        .route("/recover", post(crate::handlers::fetch_key));
+        .layer(
+            tower_http::cors::CorsLayer::new()
+                .allow_origin(tower_http::cors::Any) // TODO: Change this to a specific origin
+                .allow_methods(tower_http::cors::Any)
+                .allow_headers(tower_http::cors::Any),
+        )
+        .route("/recover", post(crate::handlers::fetch_key))
+        .layer(
+            tower_http::cors::CorsLayer::new()
+                .allow_origin(tower_http::cors::Any) // TODO: Change this to a specific origin
+                .allow_methods(tower_http::cors::Any)
+                .allow_headers(tower_http::cors::Any),
+        );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();

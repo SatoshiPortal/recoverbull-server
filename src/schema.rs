74cbb6296a48e5fe157e6a7197e5ff8ba6529b32
ethicnology <ethicnology@pm.me>
2025-01-29 12:28:20 -0500

    chore: regen schema

--- src/schema.rs
+++ src/schema.rs
@@ -1,22 +1,9 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    key (id) {
-        id -> Text,
-        created_at -> Text,
-        encrypted_backup_key -> Text,
-    }
-}
-
-diesel::table! {
     secret (id) {
         id -> Text,
         created_at -> Text,
         encrypted_secret -> Text,
     }
 }
-
-diesel::allow_tables_to_appear_in_same_query!(
-    key,
-    secret,
-);
